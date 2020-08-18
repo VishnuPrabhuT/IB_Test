dc06a917a646940a8b2e182f7d4f8238b54a970b
Vishnu - PDI <vprabh@pdisoftware.com>
2020-08-18 16:30:00 -0500

    SPA mockup

--- ant/src/App.vue
+++ ant/src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld :message="message" />
+  <div class="container">
+    <div id="table-card" class="card">
+      <v-table />
+    </div>
+    <div class="card">
+      <button class="primary">Upload</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VTable from "./components/Table.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    VTable,
   },
   data() {
     return {
@@ -17,7 +23,7 @@ export default {
     };
   },
   mounted() {
-    fetch("http://localhost:8085/")
+    fetch("http://localhost:8085/getFiles")
       .then((response) => response.text())
       .then((message) => {
         this.message = message;
@@ -25,3 +31,52 @@ export default {
   },
 };
 </script>
+
+<style lang="sass">
+*
+  padding: 0
+  margin: 0
+  box-sizing: border-box
+  outline: none
+
+html
+  padding: 0
+  margin: 0
+
+body
+  padding: 0
+  margin: 0
+  height: 100%
+  background-color: whitesmoke
+
+#app
+  display: grid
+  justify-items: center
+
+  & > .container
+    padding: 1%
+    max-width: 700px
+    width: 100%
+    display: flex
+    flex-direction: column
+    justify-content: space-around
+    background-color: #d5e4ff
+
+    & > #table-card
+      padding: 2% 2% 8% 2%
+
+    & > .card
+      border-radius: 2px
+      margin: 1%
+      padding: 2%
+      background-color: #a9c8ff
+
+      & .primary
+        cursor: pointer
+        border-radius: 7px
+        font-size: 1.4em
+        padding: 1% 7% 1% 7%
+        background-color: #497bd5
+        color: whitesmoke
+        border: 2px solid #313160
+</style>
---
+++ ant/src/components/Table.vue
@@ -0,0 +1,45 @@
+<template>
+  <table>
+    <th>Filename</th>
+    <th>Size (bytes)</th>
+    <th>Processed</th>
+
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "v-table",
+  props: {
+    message: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style lang="sass">
+table
+  width: 100%
+  border-collapse: collapse
+
+  & > th
+    font-size: 1.3em
+    font-style: bolder
+    height: 15px
+    text-align: left
+    padding: 1% 0 2% 1%
+
+table, th, td
+  border: 1px solid black
+</style>
